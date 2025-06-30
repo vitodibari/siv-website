b441f8cabf6d5fe54bf73ae90acfaa0a692337bb
Vito Di Bari <57833823+vitodibari@users.noreply.github.com>
2025-06-30 12:53:11 +0200

    gallery added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import compress from 'astro-compress';
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://summerisvolley.it',
-  vite: {
-    plugins: [tailwindcss()]
-  }
+    site: 'https://summerisvolley.it',
+
+    vite: {
+        plugins: [tailwindcss()]
+    },
+
+    integrations: [compress()]
 });
