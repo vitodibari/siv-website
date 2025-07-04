ad67e5b467f0dd209114fad4686e2b03197a590e
Vito Di Bari <57833823+vitodibari@users.noreply.github.com>
2025-07-04 16:09:55 +0200

    bugfix gallery: date filter with node backend

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,17 @@ import tailwindcss from '@tailwindcss/vite';
 
 import compress from 'astro-compress';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://summerisvolley.it',
+  site: 'https://summerisvolley.it',
 
-    vite: {
-        plugins: [tailwindcss()]
-    },
+  vite: {
+      plugins: [tailwindcss()]
+  },
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
