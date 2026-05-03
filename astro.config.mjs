45e19840783c6b8b356ea4580e95985193640966
Vito Di Bari <57833823+vitodibari@users.noreply.github.com>
2026-05-03 14:49:50 +0200

    Refactor BaseHead component and add VDB footer: enhance metadata handling and integrate sitemap support

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,15 @@ import {defineConfig} from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://summerisvolley.it',
+  site: 'https://summerisvolley.it',
 
-    vite: {
-        plugins: [tailwindcss()]
-    },
+  vite: {
+      plugins: [tailwindcss()]
+  },
+
+  integrations: [sitemap()],
 });
