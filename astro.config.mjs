e0faebffc9f6d380fa3978e3f4220be831eda487
Vito Di Bari <57833823+vitodibari@users.noreply.github.com>
2026-05-03 12:36:34 +0200

    Static gallery with date selection

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,19 +3,11 @@ import {defineConfig} from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
-import compress from 'astro-compress';
-
-import node from '@astrojs/node';
-
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://summerisvolley.it',
+    site: 'https://summerisvolley.it',
 
-  vite: {
-      plugins: [tailwindcss()]
-  },
-
-  adapter: node({
-    mode: 'standalone',
-  }),
+    vite: {
+        plugins: [tailwindcss()]
+    },
 });
