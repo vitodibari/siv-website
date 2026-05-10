90fea37ad66fefe2b050a4ee61c1961674f6cd71
Vito Di Bari <57833823+vitodibari@users.noreply.github.com>
2026-05-10 17:14:05 +0200

    Removed 2025

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,3 @@
-export const SITE_TITLE = 'Summer Is Volley 2025';
-export const SITE_DESCRIPTION = 'Sito web ufficiale del torneo Summer Is Volley 2025 organizzato da A.S.D. Iuno';
-export const SITE_KEYWORDS = 'Summer Is Volley, SummerIsVolley, Beach Volley, Torneo, 2025, Sport, A.S.D. Iuno';
+export const SITE_TITLE = 'Summer Is Volley';
+export const SITE_DESCRIPTION = 'Sito web ufficiale del torneo Summer Is Volley organizzato da A.S.D. Iuno';
+export const SITE_KEYWORDS = 'Summer Is Volley, SummerIsVolley, Beach Volley, Torneo, Sport, A.S.D. Iuno';
